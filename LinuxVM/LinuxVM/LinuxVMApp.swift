0e441050998ce003994bca6b94717fc5c6873822
Divit Shetty <divitshetty@divits-mbp-2.mynetworksettings.com>
2025-02-06 15:22:58 -0500

    Created project

---
+++ LinuxVM/LinuxVM/LinuxVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinuxVMApp.swift
+//  LinuxVM
+//
+//  Created by Divit Shetty on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct LinuxVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LinuxVM/LinuxVMTests/LinuxVMTests.swift
@@ -0,0 +1,17 @@
+//
+//  LinuxVMTests.swift
+//  LinuxVMTests
+//
+//  Created by Divit Shetty on 2/6/25.
+//
+
+import Testing
+@testable import LinuxVM
+
+struct LinuxVMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
